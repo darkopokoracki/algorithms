c39a1b63a6b46c8965d53e7b54c5432c364dca30
Darko <darkopokoracki@gmail.com>
2022-02-22 11:02:03 +0100

    Algorithms

---
+++ selection_sort.py
@@ -0,0 +1,19 @@
+numbers = [3,1,5,9,2]
+
+def selection_sort(values):
+    sorted_list = []
+
+    for i in range(0, len(values)):
+        index_to_move = index_of_min(values)
+        sorted_list.append(values.pop(index_to_move))
+ 
+    return sorted_list
+
+def index_of_min(values):
+    min_index = 0
+    for i in range(1, len(values)):
+        if values[1] < values[min_index]:
+            min_index = i
+    return min_index
+
+print(selection_sort(numbers))
